f477a4d24558a1a753b28f295897c6036f94c9bd
George Sgouros <sgouros@sgouros.gr>
2017-03-10 09:35:42 +0200

    by George Sgouros

--- start_planner.sh
+++ start_planner.sh
@@ -1,5 +1,5 @@
-cd /home/george/code/planner_design/
-nautilus &
+cd /home/george/code/planner_design
+nautilus /home/george/code/planner_design &
 git pull origin
 code
 gnome-terminal -x bash -c 'python -m SimpleHTTPServer 8080' &
